2d14a24d55bd607894e85240253594ed7d3af30c
Brian <arpaio@gmail.com>
2014-09-05 11:04:49 -0400

    fixed params for loads controller

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -69,6 +69,6 @@ class LoadsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def load_params
-      params.require(:load).permit(:origin, :destination, :user_id, :carrier_id)
+      params.require(:load).permit(:origin, :destination, :user_id, :carrier_id, :pick_date, :pick_time, :drop_date, :drop_time)
     end
 end
