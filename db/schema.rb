cdb3f2cdd054e8e0ff12121d18fd45b4dc7ab1bd
Brian Arpaio <arpaio@gmail.com>
2014-09-04 22:16:15 -0400

    added account reference to user in migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903161657) do
+ActiveRecord::Schema.define(version: 20140905020914) do
+
+  create_table "accounts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "booking_histories", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bookings", force: true do |t|
     t.integer  "carrier_id"
@@ -56,8 +66,10 @@ ActiveRecord::Schema.define(version: 20140903161657) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "account_id"
   end
 
+  add_index "users", ["account_id"], name: "index_users_on_account_id"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
