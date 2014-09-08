97a04bd2916d035a9aee8c1bd6b1b7b37839b8de
Brian Arpaio <arpaio@gmail.com>
2014-09-08 15:13:34 -0400

    added accepts nested att for loads and carriers to Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,6 @@ class Account < ActiveRecord::Base
 	has_many :loads, through :users
 	accepts_nested_attributes_for :users
 	has_many :carriers, through :users
+	accepts_nested_attributes_for :loads
+	accepts_nested_attributes_for :carriers
 end
