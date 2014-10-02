10609cd7575f81745ffc67dc84bf6480516f0f1f
Brian Arpaio <arpaio@gmail.com>
2014-10-01 20:07:01 -0400

    created join table for account and user

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,5 @@
 class Account < ActiveRecord::Base
-	has_many :users
-	accepts_nested_attributes_for :users
+	has_and_belongs_to_many :users
 	has_many :loads, through :users	
 	accepts_nested_attributes_for :loads
 	has_many :carriers, through :users	
